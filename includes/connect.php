6f21c04355386efb865a55db2eff7937d476c2f1
Mirza <mirza@Mirzas-MacBook-Pro.local>
2019-02-08 21:00:56 -0500

    updated

--- includes/connect.php
+++ includes/connect.php
@@ -4,6 +4,6 @@
 session_start();
 
 // connect to the database
-$conn = new mysqli("localhost", "roulette", "Password", "roulette");
+$conn = new mysqli("localhost", "root", "", "roulette");
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
